9b1707087bef418cb2b259365272c8ee19b99816
Geovani Nieswald <geovaninieswald@gmail.com>
2018-11-21 16:55:25 -0200

    Adicionando activity login

--- app/src/main/java/com/geovaninieswald/meusgastos/MainActivity.java
+++ app/src/main/java/com/geovaninieswald/meusgastos/MainActivity.java
@@ -1,5 +1,6 @@
 package com.geovaninieswald.meusgastos;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -40,6 +41,9 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+        startActivity(intent);
     }
 
     @Override
